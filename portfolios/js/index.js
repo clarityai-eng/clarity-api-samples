84868b2eb69b0eb032bad533e0d3f54a71cbc5ad
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-05-23 17:12:58 +0200

    feat: format js files

--- portfolios/js/index.js
+++ portfolios/js/index.js
@@ -1,20 +1,23 @@
 
 exports.portfolioRemoveShortsAndNormalizeWeights = function portfolio_remove_shorts_and_normalize(portfolio) {
-  let new_portfolio = JSON.parse(JSON.stringify(portfolio));
-  let securities = new_portfolio.securities
-  let securities_without_shorts = securities.filter(s => s.percentage > 0)
+  const new_portfolio = JSON.parse(JSON.stringify(portfolio));
+  const securities = new_portfolio.securities
+  const securities_without_shorts = securities.filter(s => s.percentage > 0)
 
-  if(securities_without_shorts.length < securities.length) {
-    const new_total_weight = securities_without_shorts
-      .map(s => s.percentage)
-      .reduce((total, current) => total + current, 0)
-
-    securities_without_shorts.forEach(s => s.percentage = parseFloat((s.percentage * 100/ new_total_weight).toFixed(4)))
-
-    new_portfolio.securities = securities_without_shorts
-    return new_portfolio
-  }
-  else {
+  if(securities_without_shorts.length >= securities.length) {
     return portfolio
   }
+
+  const new_total_weight = securities_without_shorts
+    .map(s => s.percentage)
+    .reduce((total, current) => total + current, 0)
+
+  securities_without_shorts.forEach(security => {
+    security.percentage = parseFloat((security.percentage * 100/ new_total_weight).toFixed(4))
+  })
+
+  new_portfolio.securities = securities_without_shorts
+
+  return new_portfolio
 }
+
